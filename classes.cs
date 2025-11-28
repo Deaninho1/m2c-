7697aa3a5b3efa4eff1d22b78bacd7c663f6451e
Dean <deancannavacciuolo352@gmail.com>
2025-11-28 13:08:18 +0100

    classes 1

--- classes.cs
+++ classes.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+          Program program = new Program();
+       program.Run(); //dit is een 
+    }
+
+    internal void Run()
+    {
+         
     }
 }
